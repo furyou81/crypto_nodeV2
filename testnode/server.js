83f177bc894436cd19ce27aa6d2cdabc12c9da44
Furyou81 <lfujimot@student.42.fr>
2018-07-10 19:16:23 +0000

    server

--- testnode/server.js
+++ testnode/server.js
@@ -2,7 +2,6 @@ var http = require('http');
 var url = require('url');
 var querystring = require('querystring'); // recupere params GET
 var EventEmitter = require('events').EventEmitter; // pour emettre ses propres evenements
-var bjr = require('testmodule');
 
 var jeu = new EventEmitter();
 
@@ -15,25 +14,25 @@ var server = http.createServer(function(req, res) {
 	var a = "";
 	var unknowUrl = 0;
 	res.writeHead(200, {"Content-Type": "text/html"}); // type MIME
-	if (page == '/') {
-		a = "acceuil";
-	} else if (page == "/test"){
-		a = "test";
-		jeu.emit('test', 'you loose'); // premier param = nom de l'event et on peut mettre autant de params qu'on veut (message...)
-	}
-	else if (page == "/testGET") {
-		var params = querystring.parse(url.parse(req.url).query);
-		if ('prenom' in params && 'nom' in params) { // tester si le tableaux de params contient bien ces entrees
-			a = params['prenom'] + " " + params['nom'];
-		}
-	}
-	else {
-		res.writeHead(404);
-		res.end();
-		unknowUrl = 1;
-	}
+//	if (page == '/') {
+//		a = "acceuil";
+//	} else if (page == "/test"){
+//		a = "test";
+//		jeu.emit('test', 'you loose'); // premier param = nom de l'event et on peut mettre autant de params qu'on veut (message...)
+//	}
+//	else if (page == "/testGET") {
+//		var params = querystring.parse(url.parse(req.url).query);
+//		if ('prenom' in params && 'nom' in params) { // tester si le tableaux de params contient bien ces entrees
+//			a = params['prenom'] + " " + params['nom'];
+//		}
+//	}
+//	else {
+//		res.writeHead(404);
+//		res.end();
+//		unknowUrl = 1;
+//	}
 	// res.write pour ecrire le reponse en plusieurs temps
-	if (!unknowUrl) {
+//	if (!unknowUrl) {
 		res.write('<!DOCTYPE html>' +
 		'<html>' + 
 			'<head>' +
@@ -41,12 +40,21 @@ var server = http.createServer(function(req, res) {
 				'<title> Ma page </title>' +
 			'</head>' +
 			'<body>' +
-				'<p> Hello this is <strong> html :</strong>' + a + '</p>' +
+				'<p> Hello you are on going to set the wifi for CryptoTransfer <strong> html :</strong>' + a + '</p>' +
+			'<label>Wifi Name: </label><input type="text" id="wifi_name"> </input></br>' +
+			'<label>Wifi Password: </label><input type="password" id="wifi_psw"> </input></br>' +
+			'<button onclick="setWifi();"> OK </button>' +
 			'</body>' +
+			'<script>' +
+			'function setWifi() {' +
+			'let name = document.getElementById("wifi_name").value;' +
+			'let psw = document.getElementById("wifi_psw").value;' +
+			'console.log(name + " " + psw);}' +
+			'</script>' +
 		'</html>');
 	
 		res.end(); // res.end doit tjrs etre appele en dernier pour terminer la reponse
-	}
+//	}
 });
 
 jeu.on('test', function(message) {
@@ -57,8 +65,8 @@ server.on('close', function() {
 	console.log('closing');
 })
 
-bjr.bonjour();
 server.listen(8080);
+
 
 //server.close(function() {
 //	console.log('close2');
