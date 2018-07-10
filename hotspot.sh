d91c2f6e94b2226e531a83342e2ea1e7820271fb
Furyou81 <lfujimot@student.42.fr>
2018-07-10 16:33:34 +0000

    script

---
+++ hotspot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo cp /home/pi/crypto_nodeV2/hotspot/dhcpcd.conf /etc/
+sudo cp /home/pi/crypto_nodeV2/hotspot/interfaces /etc/network/interfaces
+#sudo service dhcpcd restart
+sudo ifdown wlan0
+sudo ifup wlan0
+sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.conf /etc/hostapd/
+sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.default /etc/default/hostapd
+sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.inid.d /etc/init.d/hostapd
+sudo cp /home/pi/crypto_nodeV2/hotspot/dnsmasq.conf /etc/
+sudo reboot
+
+
+
