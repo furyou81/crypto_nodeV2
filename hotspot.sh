d31ea8da3dcabf447f43cdbdadc03a99cb164461
furyou81 <lfujimot@student.42.fr>
2018-07-23 13:47:10 +0000

    sh

--- hotspot.sh
+++ hotspot.sh
@@ -7,7 +7,7 @@ sudo ifdown wlan0
 sudo ifup wlan0
 sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.conf /etc/hostapd/
 sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.default /etc/default/hostapd
-sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.inid.d /etc/init.d/hostapd
+sudo cp /home/pi/crypto_nodeV2/hotspot/hostapd.init.d /etc/init.d/hostapd
 sudo cp /home/pi/crypto_nodeV2/hotspot/dnsmasq.conf /etc/
 sudo reboot
 
--- wifi.sh
+++ wifi.sh
@@ -3,7 +3,7 @@
 sudo cp /home/pi/crypto_nodeV2/wifi/dhcpcd.conf /etc/
 sudo cp /home/pi/crypto_nodeV2/wifi/interfaces /etc/network/interfaces
 sudo cp /home/pi/crypto_nodeV2/wifi/hostapd.default /etc/default/hostapd
-sudo cp /home/pi/crypto_nodeV2/wifi/hostapd.inid.d /etc/init.d/hostapd
+sudo cp /home/pi/crypto_nodeV2/wifi/hostapd.init.d /etc/init.d/hostapd
 sudo cp /home/pi/crypto_nodeV2/wifi/dnsmasq.conf /etc/
 sudo reboot
 
